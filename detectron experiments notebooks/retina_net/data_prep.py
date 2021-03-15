3fefb7e8351e4d83f2172b6358a302c135d57a2c
ar7 <surviva614@gmail.com>
2021-03-15 15:41:14 +0800

    retina

---
+++ detectron experiments notebooks/retina_net/data_prep.py
@@ -0,0 +1,323 @@
+import gc
+import os
+from pathlib import Path
+import random
+import sys
+
+from tqdm.notebook import tqdm
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from IPython.core.display import display, HTML
+
+# --- plotly ---
+from plotly import tools, subplots
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.io as pio
+pio.templates.default = "plotly_dark"
+
+# --- models ---
+from sklearn import preprocessing
+from sklearn.model_selection import KFold
+import lightgbm as lgb
+import xgboost as xgb
+import catboost as cb
+
+# --- setup ---
+pd.set_option('max_columns', 50)
+
+import pickle
+from pathlib import Path
+from typing import Optional
+
+import cv2
+import numpy as np
+import pandas as pd
+from detectron2.structures import BoxMode
+from tqdm import tqdm
+
+from utils import *
+
+import copy
+import logging
+
+import detectron2.data.transforms as T
+import torch
+from detectron2.data import detection_utils as utils
+
+
+import albumentations as A
+import copy
+import numpy as np
+
+import torch
+from detectron2.data import detection_utils as utils
+
+
+def get_vinbigdata_dicts(
+    imgdir: Path,
+    train_df: pd.DataFrame,
+    train_data_type: str = "original",
+    use_cache: bool = True,
+    debug: bool = True,
+    target_indices: Optional[np.ndarray] = None,
+    use_class14: bool = False,
+):
+    debug_str = f"_debug{int(debug)}"
+    train_data_type_str = f"_{train_data_type}"
+    class14_str = f"_14class{int(use_class14)}"
+    cache_path = Path(".") / f"dataset_dicts_cache{train_data_type_str}{class14_str}{debug_str}.pkl"
+    if not use_cache or not cache_path.exists():
+        print("Creating data...")
+        train_meta = pd.read_csv(imgdir / "train_meta.csv")
+        if debug:
+            train_meta = train_meta.iloc[:500]  # For debug....
+
+        # Load 1 image to get image size.
+        image_id = train_meta.loc[0, "image_id"]
+        image_path = str(imgdir / "train" / f"{image_id}.png")
+        image = cv2.imread(image_path)
+        resized_height, resized_width, ch = image.shape
+        print(f"image shape: {image.shape}")
+
+        dataset_dicts = []
+        for index, train_meta_row in tqdm(train_meta.iterrows(), total=len(train_meta)):
+            record = {}
+
+            image_id, height, width = train_meta_row.values
+            filename = str(imgdir / "train" / f"{image_id}.png")
+            record["file_name"] = filename
+            record["image_id"] = image_id
+            record["height"] = resized_height
+            record["width"] = resized_width
+            objs = []
+            for index2, row in train_df.query("image_id == @image_id").iterrows():
+                # print(row)
+                # print(row["class_name"])
+                # class_name = row["class_name"]
+                class_id = row["class_id"]
+                if class_id == 14:
+                    # It is "No finding"
+                    if use_class14:
+                        # Use this No finding class with the bbox covering all image area.
+                        bbox_resized = [0, 0, resized_width, resized_height]
+                        obj = {
+                            "bbox": bbox_resized,
+                            "bbox_mode": BoxMode.XYXY_ABS,
+                            "category_id": class_id,
+                        }
+                        objs.append(obj)
+                    else:
+                        # This annotator does not find anything, skip.
+                        pass
+                else:
+                    # bbox_original = [int(row["x_min"]), int(row["y_min"]), int(row["x_max"]), int(row["y_max"])]
+                    h_ratio = resized_height / height
+                    w_ratio = resized_width / width
+                    bbox_resized = [
+                        float(row["x_min"]) * w_ratio,
+                        float(row["y_min"]) * h_ratio,
+                        float(row["x_max"]) * w_ratio,
+                        float(row["y_max"]) * h_ratio,
+                    ]
+                    obj = {
+                        "bbox": bbox_resized,
+                        "bbox_mode": BoxMode.XYXY_ABS,
+                        "category_id": class_id,
+                    }
+                    objs.append(obj)
+            record["annotations"] = objs
+            dataset_dicts.append(record)
+        with open(cache_path, mode="wb") as f:
+            pickle.dump(dataset_dicts, f)
+
+    print(f"Load from cache {cache_path}")
+    with open(cache_path, mode="rb") as f:
+        dataset_dicts = pickle.load(f)
+    if target_indices is not None:
+        dataset_dicts = [dataset_dicts[i] for i in target_indices]
+    return dataset_dicts
+
+
+def get_vinbigdata_dicts_test(
+    imgdir: Path, test_meta: pd.DataFrame, use_cache: bool = True, debug: bool = True,
+):
+    debug_str = f"_debug{int(debug)}"
+    cache_path = Path(".") / f"dataset_dicts_cache_test{debug_str}.pkl"
+    if not use_cache or not cache_path.exists():
+        print("Creating data...")
+        # test_meta = pd.read_csv(imgdir / "test_meta.csv")
+        if debug:
+            test_meta = test_meta.iloc[:500]  # For debug....
+
+        # Load 1 image to get image size.
+        image_id = test_meta.loc[0, "image_id"]
+        image_path = str(imgdir / "test" / f"{image_id}.png")
+        image = cv2.imread(image_path)
+        resized_height, resized_width, ch = image.shape
+        print(f"image shape: {image.shape}")
+
+        dataset_dicts = []
+        for index, test_meta_row in tqdm(test_meta.iterrows(), total=len(test_meta)):
+            record = {}
+
+            image_id, height, width = test_meta_row.values
+            filename = str(imgdir / "test" / f"{image_id}.png")
+            record["file_name"] = filename
+            # record["image_id"] = index
+            record["image_id"] = image_id
+            record["height"] = resized_height
+            record["width"] = resized_width
+            # objs = []
+            # record["annotations"] = objs
+            dataset_dicts.append(record)
+        with open(cache_path, mode="wb") as f:
+            pickle.dump(dataset_dicts, f)
+
+    print(f"Load from cache {cache_path}")
+    with open(cache_path, mode="rb") as f:
+        dataset_dicts = pickle.load(f)
+    return dataset_dicts
+
+thing_classes = [
+    "Aortic enlargement",
+    "Atelectasis",
+    "Calcification",
+    "Cardiomegaly",
+    "Consolidation",
+    "ILD",
+    "Infiltration",
+    "Lung Opacity",
+    "Nodule/Mass",
+    "Other lesion",
+    "Pleural effusion",
+    "Pleural thickening",
+    "Pneumothorax",
+    "Pulmonary fibrosis"
+]
+category_name_to_id = {class_name: index for index, class_name in enumerate(thing_classes)}
+print(category_name_to_id)
+
+
+
+"""
+Referenced:
+ - https://detectron2.readthedocs.io/en/latest/tutorials/data_loading.html
+ - https://www.kaggle.com/dhiiyaur/detectron-2-compare-models-augmentation/#data
+"""
+import copy
+import logging
+
+import detectron2.data.transforms as T
+import torch
+from detectron2.data import detection_utils as utils
+
+
+class MyMapper:
+    """Mapper which uses `detectron2.data.transforms` augmentations"""
+
+    def __init__(self, cfg, is_train: bool = True):
+        aug_kwargs = cfg.aug_kwargs
+        aug_list = [
+            # T.Resize((800, 800)),
+        ]
+        if is_train:
+            aug_list.extend([getattr(T, name)(**kwargs) for name, kwargs in aug_kwargs.items()])
+        self.augmentations = T.AugmentationList(aug_list)
+        self.is_train = is_train
+
+        mode = "training" if is_train else "inference"
+        print(f"[MyDatasetMapper] Augmentations used in {mode}: {self.augmentations}")
+
+    def __call__(self, dataset_dict):
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        image = utils.read_image(dataset_dict["file_name"], format="BGR")
+
+        aug_input = T.AugInput(image)
+        transforms = self.augmentations(aug_input)
+        image = aug_input.image
+
+        # if not self.is_train:
+        #     # USER: Modify this if you want to keep them for some reason.
+        #     dataset_dict.pop("annotations", None)
+        #     dataset_dict.pop("sem_seg_file_name", None)
+        #     return dataset_dict
+
+        image_shape = image.shape[:2]  # h, w
+        dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+        annos = [
+            utils.transform_instance_annotations(obj, transforms, image_shape)
+            for obj in dataset_dict.pop("annotations")
+            if obj.get("iscrowd", 0) == 0
+        ]
+        instances = utils.annotations_to_instances(annos, image_shape)
+        dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        return dataset_dict
+    
+    
+   
+
+"""
+Referenced:
+ - https://detectron2.readthedocs.io/en/latest/tutorials/data_loading.html
+ - https://www.kaggle.com/dhiiyaur/detectron-2-compare-models-augmentation/#data
+"""
+
+class AlbumentationsMapper:
+    """Mapper which uses `albumentations` augmentations"""
+    def __init__(self, cfg, is_train: bool = True):
+        aug_kwargs = cfg.aug_kwargs
+        aug_list = [
+        ]
+        if is_train:
+            aug_list.extend([getattr(A, name)(**kwargs) for name, kwargs in aug_kwargs.items()])
+        self.transform = A.Compose(
+            aug_list, bbox_params=A.BboxParams(format="pascal_voc", label_fields=["category_ids"])
+        )
+        self.is_train = is_train
+
+        mode = "training" if is_train else "inference"
+        print(f"[AlbumentationsMapper] Augmentations used in {mode}: {self.transform}")
+
+    def __call__(self, dataset_dict):
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        image = utils.read_image(dataset_dict["file_name"], format="BGR")
+
+        # aug_input = T.AugInput(image)
+        # transforms = self.augmentations(aug_input)
+        # image = aug_input.image
+
+        prev_anno = dataset_dict["annotations"]
+        bboxes = np.array([obj["bbox"] for obj in prev_anno], dtype=np.float32)
+        # category_id = np.array([obj["category_id"] for obj in dataset_dict["annotations"]], dtype=np.int64)
+        category_id = np.arange(len(dataset_dict["annotations"]))
+
+        transformed = self.transform(image=image, bboxes=bboxes, category_ids=category_id)
+        image = transformed["image"]
+        annos = []
+        for i, j in enumerate(transformed["category_ids"]):
+            d = prev_anno[j]
+            d["bbox"] = transformed["bboxes"][i]
+            annos.append(d)
+        dataset_dict.pop("annotations", None)  # Remove unnecessary field.
+
+        # if not self.is_train:
+        #     # USER: Modify this if you want to keep them for some reason.
+        #     dataset_dict.pop("annotations", None)
+        #     dataset_dict.pop("sem_seg_file_name", None)
+        #     return dataset_dict
+
+        image_shape = image.shape[:2]  # h, w
+        dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+        instances = utils.annotations_to_instances(annos, image_shape)
+        dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        return dataset_dict
---
+++ detectron experiments notebooks/retina_net/main.py
@@ -0,0 +1,206 @@
+import argparse
+import dataclasses
+import json
+import os
+import pickle
+import random
+import sys
+from dataclasses import dataclass
+from distutils.util import strtobool
+from pathlib import Path
+
+import cv2
+import detectron2
+import numpy as np
+import pandas as pd
+import torch
+from detectron2 import model_zoo
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.engine import DefaultPredictor, DefaultTrainer, launch
+from detectron2.evaluation import COCOEvaluator
+from detectron2.structures import BoxMode
+from detectron2.utils.logger import setup_logger
+from detectron2.utils.visualizer import Visualizer
+from tqdm import tqdm
+
+setup_logger()
+
+# --- flags ---
+from dataclasses import dataclass, field
+from typing import Dict
+
+from detectron2.config.config import CfgNode as CN
+
+from data_prep import *
+from utils import *
+from train import *
+
+
+
+@dataclass
+class Flags:
+    # General
+    debug: bool = True
+    outdir: str = "results/det"
+
+    # Data config
+    imgdir_name: str = "vinbigdata-chest-xray-resized-png-256x256"
+    split_mode: str = "all_train"  # all_train or valid20
+    seed: int = 111
+    train_data_type: str = "original"  # original or wbf
+    use_class14: bool = False
+    # Training config
+    iter: int = 10000
+    ims_per_batch: int = 2  # images per batch, this corresponds to "total batch size"
+    num_workers: int = 4
+    lr_scheduler_name: str = "WarmupMultiStepLR"  # WarmupMultiStepLR (default) or WarmupCosineLR
+    base_lr: float = 0.00025
+    roi_batch_size_per_image: int = 512
+    eval_period: int = 10000
+    aug_kwargs: Dict = field(default_factory=lambda: {})
+
+    def update(self, param_dict: Dict) -> "Flags":
+        # Overwrite by `param_dict`
+        for key, value in param_dict.items():
+            if not hasattr(self, key):
+                raise ValueError(f"[ERROR] Unexpected key for flag = {key}")
+            setattr(self, key, value)
+        return self
+    
+    
+flags_dict = {
+    "debug": False,
+    "outdir": "results/v9", 
+    "imgdir_name": "vinbigdata",
+    "split_mode": "valid20",
+    "iter": 15000,
+    "roi_batch_size_per_image": 512,
+    "eval_period": 1000,
+    "lr_scheduler_name": "WarmupCosineLR",
+    "base_lr": 0.005,
+    "num_workers": 4,
+    "aug_kwargs": {
+        "HorizontalFlip": {"p": 0.5},
+        "ShiftScaleRotate": {"scale_limit": 0.15, "rotate_limit": 10, "p": 0.5},
+        "RandomBrightnessContrast": {"p": 0.5}
+    }
+}
+
+
+
+# args = parse()
+print("torch", torch.__version__)
+flags = Flags().update(flags_dict)
+print("flags", flags)
+debug = flags.debug
+outdir = Path(flags.outdir)
+os.makedirs(str(outdir), exist_ok=True)
+flags_dict = dataclasses.asdict(flags)
+save_yaml(outdir / "flags.yaml", flags_dict)
+
+# --- Read data ---
+inputdir = Path("/kaggle/input")
+datadir = inputdir / "vinbigdata-chest-xray-abnormalities-detection"
+imgdir = inputdir / flags.imgdir_name
+
+# Read in the data CSV files
+train_df = pd.read_csv(datadir / "train.csv")
+train = train_df  # alias
+# sample_submission = pd.read_csv(datadir / 'sample_submission.csv')
+
+
+
+train_data_type = flags.train_data_type
+if flags.use_class14:
+    thing_classes.append("No finding")
+
+split_mode = flags.split_mode
+if split_mode == "all_train":
+    DatasetCatalog.register(
+        "vinbigdata_train",
+        lambda: get_vinbigdata_dicts(
+            imgdir, train_df, train_data_type, debug=debug, use_class14=flags.use_class14
+        ),
+    )
+    MetadataCatalog.get("vinbigdata_train").set(thing_classes=thing_classes)
+elif split_mode == "valid20":
+    # To get number of data...
+    n_dataset = len(
+        get_vinbigdata_dicts(
+            imgdir, train_df, train_data_type, debug=debug, use_class14=flags.use_class14
+        )
+    )
+    n_train = int(n_dataset * 0.8)
+    print("n_dataset", n_dataset, "n_train", n_train)
+    rs = np.random.RandomState(flags.seed)
+    inds = rs.permutation(n_dataset)
+    train_inds, valid_inds = inds[:n_train], inds[n_train:]
+    DatasetCatalog.register(
+        "vinbigdata_train",
+        lambda: get_vinbigdata_dicts(
+            imgdir,
+            train_df,
+            train_data_type,
+            debug=debug,
+            target_indices=train_inds,
+            use_class14=flags.use_class14,
+        ),
+    )
+    MetadataCatalog.get("vinbigdata_train").set(thing_classes=thing_classes)
+    DatasetCatalog.register(
+        "vinbigdata_valid",
+        lambda: get_vinbigdata_dicts(
+            imgdir,
+            train_df,
+            train_data_type,
+            debug=debug,
+            target_indices=valid_inds,
+            use_class14=flags.use_class14,
+        ),
+    )
+    MetadataCatalog.get("vinbigdata_valid").set(thing_classes=thing_classes)
+else:
+    raise ValueError(f"[ERROR] Unexpected value split_mode={split_mode}")
+    
+    
+dataset_dicts = get_vinbigdata_dicts(imgdir, train, debug=debug)
+
+
+cfg = get_cfg()
+cfg.aug_kwargs = CN(flags.aug_kwargs)  # pass aug_kwargs to cfg
+
+original_output_dir = cfg.OUTPUT_DIR
+cfg.OUTPUT_DIR = str(outdir)
+print(f"cfg.OUTPUT_DIR {original_output_dir} -> {cfg.OUTPUT_DIR}")
+
+config_name = "COCO-Detection/retinanet_R_50_FPN_3x.yaml"
+cfg.merge_from_file(model_zoo.get_config_file(config_name))
+cfg.DATASETS.TRAIN = ("vinbigdata_train",)
+if split_mode == "all_train":
+    cfg.DATASETS.TEST = ()
+else:
+    cfg.DATASETS.TEST = ("vinbigdata_valid",)
+    cfg.TEST.EVAL_PERIOD = flags.eval_period
+
+cfg.DATALOADER.NUM_WORKERS = flags.num_workers
+# Let training initialize from model zoo
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(config_name)
+cfg.SOLVER.IMS_PER_BATCH = flags.ims_per_batch
+cfg.SOLVER.LR_SCHEDULER_NAME = flags.lr_scheduler_name
+cfg.SOLVER.BASE_LR = flags.base_lr  # pick a good LR
+cfg.SOLVER.MAX_ITER = flags.iter
+cfg.SOLVER.CHECKPOINT_PERIOD = 100000  # Small value=Frequent save need a lot of storage.
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = flags.roi_batch_size_per_image
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(thing_classes)
+# NOTE: this config means the number of classes,
+# but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+
+trainer = MyTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+
