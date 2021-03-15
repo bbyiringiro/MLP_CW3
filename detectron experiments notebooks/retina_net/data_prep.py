c1393d9181335b4cc2989e02802e0cf2a94aac7a
ar7 <surviva614@gmail.com>
2021-03-15 16:04:42 +0800

    retina

--- detectron experiments notebooks/retina_net/data_prep.py
+++ detectron experiments notebooks/retina_net/data_prep.py
@@ -15,15 +15,15 @@ import seaborn as sns
 
 # from IPython.core.display import display, HTML
 
-# --- plotly ---
-from plotly import tools, subplots
-import plotly.offline as py
-py.init_notebook_mode(connected=True)
-import plotly.graph_objs as go
-import plotly.express as px
-import plotly.figure_factory as ff
-import plotly.io as pio
-pio.templates.default = "plotly_dark"
+# # --- plotly ---
+# from plotly import tools, subplots
+# import plotly.offline as py
+# py.init_notebook_mode(connected=True)
+# import plotly.graph_objs as go
+# import plotly.express as px
+# import plotly.figure_factory as ff
+# import plotly.io as pio
+# pio.templates.default = "plotly_dark"
 
 # --- models ---
 from sklearn import preprocessing
